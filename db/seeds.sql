ad85815409c811535c5c28fbd4b55a8777580144
Nick Romano <njr7romano@yahoo.com>
2020-05-15 13:34:05 -0400

    Initial files put in place. Select all burgers to devour working

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Flamin Hot Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Southwest Ranchero Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('A1 Steakhouse Burger', false);
